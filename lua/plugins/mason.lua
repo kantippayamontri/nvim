184550ce8ecacd7bde09d57a70e4a422673d6d42
Kan <kantippayamontri@gmail.com>
2025-01-06 21:24:26 +0700

    nothing new

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -23,6 +23,9 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     lazy = false,
+    opts = {
+      auto_install = true,
+    },
     config = function()
       require("mason-lspconfig").setup({
         -- ensure_installed = { "lua_ls", "pylsp", "html", "tsserver", "cssmodules_ls" },
