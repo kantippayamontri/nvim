1400bc5019cd327474ef20b278d6efe3eb6c595b
Kan <kantippayamontri@gmail..com>
2025-01-30 14:10:38 +0700

    add keymap to restart lsp server with <leader>lr

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,23 +11,11 @@ vim.keymap.set("n", "'", function()
   vim.cmd.norm("gcc")
 end)
 
--- vim.keymap.set("n", "<leader>fj", function()
---   vim.cmd.norm("<leader>sg")
--- end)
---
--- vim.keymap.set("n", "<leader>fJ", function()
---   vim.cmd.norm("<leader>sG")
--- end)
-
--- for tab binding
--- vim.keymap.set("n", "<S-t>", function()
---   vim.cmd.norm(":tabn<CR>")
--- end)
--- vim.keymap.set("n", "<S-Tab>", function()
---   vim.cmd.norm(":tabp<CR>")
--- end)
-
+-- for neovim tab
 vim.api.nvim_set_keymap("n", "<Tab>", ":tabn<CR>", { noremap = true, silent = true }) -- go to next tab
 vim.api.nvim_set_keymap("n", "<S-Tab>", ":tabp<CR>", { noremap = true, silent = true }) -- go to previous tab
 vim.api.nvim_set_keymap("n", "<leader>tn", ":tabnew<CR>", { noremap = true, silent = true }) -- create new tab
 vim.api.nvim_set_keymap("n", "<leader>tc", ":tabclose<CR>", { noremap = true, silent = true }) -- close the tab
+
+-- for neovim restart lsp server
+vim.api.nvim_set_keymap("n", "<leader>lr", ":LspRestart<CR>", {desc = "Restart LSP sercer"})
