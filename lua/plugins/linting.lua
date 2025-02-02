fc90162c86e45c8f4879fd4f0784af1c6251b291
Kan <kantippayamontri@gmail.com>
2025-02-02 16:32:14 +0700

    comment the problem file

--- lua/plugins/linting.lua
+++ lua/plugins/linting.lua
@@ -23,7 +23,7 @@ return {
           lint.try_lint()
         end,
       })
-      vim.keymap.set("n", "<leader>l", function()
+      vim.keymap.set("n", "<leader>ll", function()
         lint.try_lint()
       end, { desc = "Trigger linting for current file" })
     end,
