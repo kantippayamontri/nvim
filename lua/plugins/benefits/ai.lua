37f38803fe017d7b6125b9a92794028bcd9e775c
Kan <kantippayamontri@gmail.com>
2025-02-20 19:45:45 +0700

    add augment ai in ai.lua

--- lua/plugins/benefits/ai.lua
+++ lua/plugins/benefits/ai.lua
@@ -1,5 +1,5 @@
 return {
-  { "github/copilot.vim", enabled = true },
+  { "github/copilot.vim", enabled = false },
   {
     "luozhiya/fittencode.nvim",
     enabled = false,
@@ -160,6 +160,21 @@ return {
           max_size = 10,
         },
       })
+    end,
+  },
+  {
+    "https://github.com/augmentcode/augment.vim",
+    enabled = true,
+    config = function()
+      -- start chat in normal and visual mode
+      vim.api.nvim_set_keymap("n", "<leader>ac", ":Augment chat<CR>", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap("v", "<leader>ac", ":Augment chat<CR>", { noremap = true, silent = true })
+      -- start chat with new conversation
+      vim.api.nvim_set_keymap("n", "<leader>an", ":Augment chat-new<CR>", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap("v", "<leader>an", ":Augment chat-new<CR>", { noremap = true, silent = true })
+      -- toggle chat window
+      vim.api.nvim_set_keymap("n", "<leader>at", ":Augment chat-toggle<CR>", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap("v", "<leader>at", ":Augment chat-toggle<CR>", { noremap = true, silent = true })
     end,
   },
 }
